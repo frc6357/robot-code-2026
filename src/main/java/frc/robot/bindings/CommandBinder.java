e89d38509e799809c8351c5b74be1faa644d4851
scootar <gavinmacalpine@gmail.com>
2026-01-10 20:32:47 -0600

    oopsies I deleted everything! just trust the process

--- src/main/java/frc/robot/bindings/CommandBinder.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.bindings;
-
-/** An interface that defines a method on how to bind buttons for each subsystem */
-public interface CommandBinder
-{
-    public void bindButtons();
-}
