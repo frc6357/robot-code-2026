64c0ff8ee48dd1a25cb582272a7a75b712347a4a
scootar <gavinmacalpine@gmail.com>
2026-01-11 21:39:57 -0600

    small adjustment

--- src/main/java/frc/robot/subsystems/SKLauncher.java
+++ src/main/java/frc/robot/subsystems/SKLauncher.java
@@ -2,16 +2,19 @@ package frc.robot.subsystems;
 
 import static frc.robot.Ports.LauncherPorts.kLauncherMotor;
 
-import com.revrobotics.spark.SparkFlex;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SKLauncher extends SubsystemBase {
 
+    //TODO: Moved motor variable declaration outside of constructor since it will be needed in other methods
+    //TODO: Changed motor type to TalonFX since we're using krakens
+    private TalonFX motor;
+
     public SKLauncher() {
         //initialize motor objects
-        SparkFlex motor = new SparkFlex(kLauncherMotor.ID, MotorType.kBrushless);
+        motor = new TalonFX(kLauncherMotor.ID);
     }
 
     public void initialize() {
