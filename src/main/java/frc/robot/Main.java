f65053422faf76b098144bc4a01ea07a061c55f8
scootar <gavinmacalpine@gmail.com>
2026-02-21 23:51:42 -0600

    Integrated AdvantageKit and included a robot mode chooser on init

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import java.util.Scanner;
+
 import edu.wpi.first.wpilibj.RobotBase;
 
 /**
@@ -12,14 +14,36 @@ import edu.wpi.first.wpilibj.RobotBase;
  * call.
  */
 public final class Main {
-  private Main() {}
+    private Main() {}
 
-  /**
-   * Main initialization function. Do not perform any initialization here.
-   *
-   * <p>If you change your main robot class, change the parameter type.
-   */
-  public static void main(String... args) {
-    RobotBase.startRobot(Robot::new);
-  }
+    /**
+     * Main initialization function. Do not perform any initialization here.
+     *
+     * <p>If you change your main robot class, change the parameter type.
+     */
+    public static void main(String... args) {
+        int mode = 0;
+        Scanner scanner = new Scanner(System.in);
+        while(mode != 1 && mode != 2) {
+            System.out.print("Select robot mode (1. Controlled, 2. Replay): ");
+        try{
+            mode = scanner.nextInt();
+        }
+        catch(Exception e){
+            mode = 0;
+            System.out.println("Invalid input. Please enter 1 or 2.");
+            scanner.nextLine(); // Clear the invalid input
+        }
+        }
+
+        final int selectedMode = mode;
+        if(selectedMode == 1) {
+            System.out.println("Starting in CONTROLLED mode.");
+            scanner.close();  // Can close System.in since AdvantageKit won't need it in CONTROLLED mode
+        }
+        else {
+            System.out.println("Starting in REPLAY mode.");
+        }
+        RobotBase.startRobot(() -> new Robot(selectedMode == 1 ? Robot.RobotMode.CONTROLLED : Robot.RobotMode.REPLAY));
+    }
 }
