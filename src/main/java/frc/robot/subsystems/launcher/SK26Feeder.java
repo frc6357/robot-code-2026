a3dadafc242b87d1638b290a54b35b9ccd9eda58
ERobin07 <evantheboywonder@outlook.com>
2026-03-04 17:38:11 -0600

    Small tweaks

--- src/main/java/frc/robot/subsystems/launcher/SK26Feeder.java
+++ src/main/java/frc/robot/subsystems/launcher/SK26Feeder.java
@@ -15,23 +15,23 @@ public class SK26Feeder extends SubsystemBase{
 
     SparkFlex feedermotor;
 
-    public SK26Feeder() {
-
+    public SK26Feeder() 
+    {
         feedermotor = new SparkFlex(kFeederMotor.ID, MotorType.kBrushless);
         SparkFlexConfig config = new SparkFlexConfig();
         feedermotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     }
 
-    public void runFeeder() {
+    public void runFeeder() 
+    {
         feedermotor.setVoltage(kFeederVoltage);
     }
 
-    public void stopFeeder() {
+    public void stopFeeder() 
+    {
         feedermotor.setVoltage(0.0);
     }
 
     @Override
-    public void periodic() {
-        
-    }
+    public void periodic() {}
 }
