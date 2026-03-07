fd0d0455f1c3933e9f6c10be9a78bf69fd46cfee
Jacob Lubecki <jacoblubecki@gmail.com>
2026-03-07 17:39:37 -0600

    Cache DriveIOCTRE signals and use explicit refresh only

--- src/main/java/frc/robot/subsystems/drive/DriveIOCTRE.java
+++ src/main/java/frc/robot/subsystems/drive/DriveIOCTRE.java
@@ -1,48 +1,92 @@
 package frc.robot.subsystems.drive;
 
-import com.ctre.phoenix6.hardware.Pigeon2;
-import com.ctre.phoenix6.swerve.SwerveModule;
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
 
 import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
 
 public class DriveIOCTRE implements DriveIO {
-    private final GeneratedDrivetrain drivetrain;
+    private final StatusSignal<Angle> pitchSignal;
+    private final StatusSignal<AngularVelocity> pitchVelocitySignal;
+    private final StatusSignal<Angle> rollSignal;
+    private final StatusSignal<AngularVelocity> rollVelocitySignal;
 
+    private final StatusSignal<Voltage>[] driveVoltageSignals;
+    private final StatusSignal<Voltage>[] steerVoltageSignals;
+    private final StatusSignal<Current>[] driveStatorCurrentSignals;
+    private final StatusSignal<Current>[] steerStatorCurrentSignals;
+    private final StatusSignal<Current>[] driveSupplyCurrentSignals;
+    private final StatusSignal<Current>[] steerSupplyCurrentSignals;
+    private final StatusSignal<Temperature>[] driveTempSignals;
+    private final StatusSignal<Temperature>[] steerTempSignals;
+
+    private final BaseStatusSignal[] allSignals;
+
+    @SuppressWarnings("unchecked")
     public DriveIOCTRE(GeneratedDrivetrain drivetrain) {
-        this.drivetrain = drivetrain;
+        var pigeon = drivetrain.getPigeon2();
+        pitchSignal = pigeon.getPitch(false);
+        pitchVelocitySignal = pigeon.getAngularVelocityYDevice(false);
+        rollSignal = pigeon.getRoll(false);
+        rollVelocitySignal = pigeon.getAngularVelocityXDevice(false);
+
+        var modules = drivetrain.getModules();
+        driveVoltageSignals = new StatusSignal[4];
+        steerVoltageSignals = new StatusSignal[4];
+        driveStatorCurrentSignals = new StatusSignal[4];
+        steerStatorCurrentSignals = new StatusSignal[4];
+        driveSupplyCurrentSignals = new StatusSignal[4];
+        steerSupplyCurrentSignals = new StatusSignal[4];
+        driveTempSignals = new StatusSignal[4];
+        steerTempSignals = new StatusSignal[4];
+
+        for (int i = 0; i < 4; i++) {
+            driveVoltageSignals[i] = modules[i].getDriveMotor().getMotorVoltage(false);
+            steerVoltageSignals[i] = modules[i].getSteerMotor().getMotorVoltage(false);
+            driveStatorCurrentSignals[i] = modules[i].getDriveMotor().getStatorCurrent(false);
+            steerStatorCurrentSignals[i] = modules[i].getSteerMotor().getStatorCurrent(false);
+            driveSupplyCurrentSignals[i] = modules[i].getDriveMotor().getSupplyCurrent(false);
+            steerSupplyCurrentSignals[i] = modules[i].getSteerMotor().getSupplyCurrent(false);
+            driveTempSignals[i] = modules[i].getDriveMotor().getDeviceTemp(false);
+            steerTempSignals[i] = modules[i].getSteerMotor().getDeviceTemp(false);
+        }
+
+        allSignals = new BaseStatusSignal[] {
+            pitchSignal, pitchVelocitySignal, rollSignal, rollVelocitySignal,
+            driveVoltageSignals[0], driveVoltageSignals[1], driveVoltageSignals[2], driveVoltageSignals[3],
+            steerVoltageSignals[0], steerVoltageSignals[1], steerVoltageSignals[2], steerVoltageSignals[3],
+            driveStatorCurrentSignals[0], driveStatorCurrentSignals[1], driveStatorCurrentSignals[2], driveStatorCurrentSignals[3],
+            steerStatorCurrentSignals[0], steerStatorCurrentSignals[1], steerStatorCurrentSignals[2], steerStatorCurrentSignals[3],
+            driveSupplyCurrentSignals[0], driveSupplyCurrentSignals[1], driveSupplyCurrentSignals[2], driveSupplyCurrentSignals[3],
+            steerSupplyCurrentSignals[0], steerSupplyCurrentSignals[1], steerSupplyCurrentSignals[2], steerSupplyCurrentSignals[3],
+            driveTempSignals[0], driveTempSignals[1], driveTempSignals[2], driveTempSignals[3],
+            steerTempSignals[0], steerTempSignals[1], steerTempSignals[2], steerTempSignals[3],
+        };
     }
 
     @Override
     public void updateInputs(DriveIOInputs inputs) {
-        Pigeon2 pigeon = drivetrain.getPigeon2();
+        BaseStatusSignal.refreshAll(allSignals);
 
-        inputs.pitchDegrees = pigeon.getPitch().getValue().in(Units.Degrees);
-        inputs.pitchVelocityDegreesPerSecond =
-                pigeon.getAngularVelocityYDevice().getValue().in(Units.DegreesPerSecond);
-        inputs.rollDegrees = pigeon.getRoll().getValue().in(Units.Degrees);
-        inputs.rollVelocityDegreesPerSecond =
-                pigeon.getAngularVelocityXDevice().getValue().in(Units.DegreesPerSecond);
+        inputs.pitchDegrees = pitchSignal.getValue().in(Units.Degrees);
+        inputs.pitchVelocityDegreesPerSecond = pitchVelocitySignal.getValue().in(Units.DegreesPerSecond);
+        inputs.rollDegrees = rollSignal.getValue().in(Units.Degrees);
+        inputs.rollVelocityDegreesPerSecond = rollVelocitySignal.getValue().in(Units.DegreesPerSecond);
 
-        var modules = drivetrain.getModules();
         for (int i = 0; i < 4; i++) {
-            SwerveModule<?, ?, ?> module = modules[i];
-
-            inputs.driveVolts[i] =
-                    module.getDriveMotor().getMotorVoltage().getValue().in(Units.Volts);
-            inputs.steerVolts[i] =
-                    module.getSteerMotor().getMotorVoltage().getValue().in(Units.Volts);
-            inputs.driveStatorCurrentAmps[i] =
-                    module.getDriveMotor().getStatorCurrent().getValue().in(Units.Amps);
-            inputs.steerStatorCurrentAmps[i] =
-                    module.getSteerMotor().getStatorCurrent().getValue().in(Units.Amps);
-            inputs.driveSupplyCurrentAmps[i] =
-                    module.getDriveMotor().getSupplyCurrent().getValue().in(Units.Amps);
-            inputs.steerSupplyCurrentAmps[i] =
-                    module.getSteerMotor().getSupplyCurrent().getValue().in(Units.Amps);
-            inputs.driveTempCelsius[i] =
-                    module.getDriveMotor().getDeviceTemp().getValue().in(Units.Celsius);
-            inputs.steerTempCelsius[i] =
-                    module.getSteerMotor().getDeviceTemp().getValue().in(Units.Celsius);
+            inputs.driveVolts[i] = driveVoltageSignals[i].getValue().in(Units.Volts);
+            inputs.steerVolts[i] = steerVoltageSignals[i].getValue().in(Units.Volts);
+            inputs.driveStatorCurrentAmps[i] = driveStatorCurrentSignals[i].getValue().in(Units.Amps);
+            inputs.steerStatorCurrentAmps[i] = steerStatorCurrentSignals[i].getValue().in(Units.Amps);
+            inputs.driveSupplyCurrentAmps[i] = driveSupplyCurrentSignals[i].getValue().in(Units.Amps);
+            inputs.steerSupplyCurrentAmps[i] = steerSupplyCurrentSignals[i].getValue().in(Units.Amps);
+            inputs.driveTempCelsius[i] = driveTempSignals[i].getValue().in(Units.Celsius);
+            inputs.steerTempCelsius[i] = steerTempSignals[i].getValue().in(Units.Celsius);
         }
     }
 }
