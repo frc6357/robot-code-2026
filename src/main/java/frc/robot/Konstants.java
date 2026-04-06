2da27c84ec28e0757d3a1478be3f907996cf1997
scootar <gavinmacalpine@gmail.com>
2026-04-06 16:25:40 -0500

    sotm testing

--- src/main/java/frc/robot/Konstants.java
+++ src/main/java/frc/robot/Konstants.java
@@ -554,7 +554,7 @@ public final class Konstants
             );
 
         // Phase delay compensation
-        public static final double kPhaseDelaySeconds = 0.2;
+        public static final double kPhaseDelaySeconds = -1.2;
 
         // Launch angle mode
         public enum LaunchAngleMode { FIXED, ADJUSTABLE }
